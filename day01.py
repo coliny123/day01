b638614f20c7bb582ba9a2748d87758a32eb7414
dohwan <dh990921@naver.com>
2023-01-12 16:22:15 +0900

    gui

--- day01.py
+++ day01.py
@@ -1,13 +1,6 @@
-subjects = {
-    "의영": "a+",
-    "오래된 미래": "b+",
-    "양자역학": "a0",
-}
-student = "김도훈"
-subject = "오래된 미래"
-#old style
-print("%s 학생의 %s 과목 성적은 %s 입니다." % (student, subject, subjects[subject]))
-#modern style(forma함수)
-print("{} 학생의 {} 과목 성적은 {} 입니다.".format(student, subject, subjects[subject]))
-#ultra modern
-print(f"{student} 학생의 {subject} 과목 성적은 {subjects[subject]} 입니다.")
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry("400x300")
+win.title("파이썬 1일차 preview")
+win.mainloop()
